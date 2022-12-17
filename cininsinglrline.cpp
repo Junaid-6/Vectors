b02fb8c516aa250f710c5f2ca209942d29c78592
Muhammad Junaid Ahmad <114339174+Junaid-6@users.noreply.github.com>
2022-12-18 00:24:27 +0500

    Add files via upload

---
+++ cininsinglrline.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x,y,z; 
+    char separator;
+    cin >> x >> separator&&separator=='/' && cin >> y >> separator&&separator=='/' && cin >> z ;
+    cout<<x<<"  "<<y<<"   "<<z;
+    return 0;
+}
